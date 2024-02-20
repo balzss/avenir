dc77f80e7d1144755084d57b3e501292820cdf91
balazs.saros <balazs.saros@gmail.com>
2024-02-20 17:30:54 +0100

    manual sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), sitemap(), compress()],
+  integrations: [tailwind(), compress()],
   site: 'https://avenirvision.hu'
 });
