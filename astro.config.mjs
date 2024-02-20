3881fb4e0447c6b12dee738799a3fe305cbf8513
balazs.saros <balazs.saros@gmail.com>
2024-02-20 17:06:44 +0100

    add sitemap gen

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [tailwind(), compress()],
-  site: 'https://avenirvision.hu',
+  integrations: [tailwind(), sitemap(), compress()],
+  site: 'https://avenirvision.hu'
 });
